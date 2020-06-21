0ac5049713002cc67e52e38985eabdac37a302fb
Andrei Stoian <stoianandrei980@gmail.com>
2020-06-21 16:24:28 +0300

    layout and buttons done

---
+++ src/main/Controller.java
@@ -0,0 +1,61 @@
+package main;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void btnOnePressed(ActionEvent actionEvent) {
+
+    }
+
+    public void btnTwoPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnThreePressed(ActionEvent actionEvent) {
+    }
+
+    public void btnPlusPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnFourPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnFivePressed(ActionEvent actionEvent) {
+    }
+
+    public void btnSixPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnSevenPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnEightPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnNinePressed(ActionEvent actionEvent) {
+    }
+
+    public void btnZeroPressed(ActionEvent actionEvent) {
+    }
+
+
+    public void btnMinusPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnMultiplyPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnEqualPressed(ActionEvent actionEvent) {
+    }
+
+
+
+    public void btnDividePressed(ActionEvent actionEvent) {
+    }
+
+    public void btnDotPressed(ActionEvent actionEvent) {
+    }
+
+    public void btnCLPressed(ActionEvent actionEvent) {
+    }
+}
