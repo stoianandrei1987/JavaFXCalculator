4dcfba34017feb22557b4444f68fbb8fb392ba76
Andrei Stoian <stoianandrei980@gmail.com>
2020-06-21 16:29:35 +0300

    switched to Maven project

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/calculator.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/calculator.fxml"));
         primaryStage.setTitle("JavaFX Calculator");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
